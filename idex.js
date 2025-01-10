386be9ba90fba84f2924e54d30776c9bca698938
Deepak Kumar <dk6061051@gmail.com>
2025-01-10 23:40:52 +0530

    adding js file

---
+++ idex.js
@@ -0,0 +1,47 @@
+// var a = 10;
+// var a = 20;
+// console.log(a)
+
+// let b=20;
+//  b=30;
+// console.log(a)
+
+// const c = 10;
+// console.log(b)
+
+// var a;
+// a=10;
+// console.log(a)
+
+// let b;
+// b=20;
+// console.log(b)
+ 
+// const c;
+// c=5;
+// console.log(c)
+ 
+// console.log(a)
+// var a=20;
+
+
+// console.log(a)
+// const a=20;
+
+// var a=50;
+// {
+//     console.log(a)
+//     var a=70;
+
+// }
+// console.log(a)
+
+// var a=50;
+// function one(){
+//     console.log(a)
+//     var a=30;
+// }
+// one()
+// console.log(a)
+
+
